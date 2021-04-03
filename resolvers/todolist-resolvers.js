03f8e8b2229540d867ff13ea00d681ba4359eb5b
SimoneMesiha <53223014+SimoneMesiha@users.noreply.github.com>
2021-04-03 13:27:18 -0400

    fixed bug

--- resolvers/todolist-resolvers.js
+++ resolvers/todolist-resolvers.js
@@ -41,13 +41,24 @@ module.exports = {
 			const objectId = new ObjectId();
 			const found = await Todolist.findOne({_id: listId});
 			if(!found) return ('Todolist not found');
-			item._id = objectId;
+
+
+
+			//item._id = objectId;							//To fix the bug
+			/////////////////////
+			if(item._id ===''){
+				item.id = objectId;
+			}
+			///////////////////
+
+
 			let listItems = found.items;
 			listItems.push(item);
 			
 			const updated = await Todolist.updateOne({_id: listId}, { items: listItems });
 
-			if(updated) return (objectId);
+			//if(updated) return (objectId); 
+			if(updated) return (item._id);
 			else return ('Could not add item');
 		},
 		/** 
